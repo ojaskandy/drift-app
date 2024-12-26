00a29379935a226db11c3737d48bf66c788aa45e
ojaskandy <ojaskandy@gmail.com>
2024-12-25 18:54:03 -0800

    changed backend to get rid of localhost 3000 in cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware: CORS Configuration
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Dynamic frontend URL
+    origin: process.env.FRONTEND_URL, // Dynamic frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
